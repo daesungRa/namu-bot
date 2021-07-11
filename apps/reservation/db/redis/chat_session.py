f121c36821fef337179c61970196fc1b76184be4
daesungra <daesungra@gmail.com>
2021-07-11 16:11:09 +0900

    db-redis: Add instance variables and logging to redis chat_session

--- apps/reservation/db/redis/chat_session.py
+++ apps/reservation/db/redis/chat_session.py
@@ -3,34 +3,55 @@ Redis session object for chat.
 MAINTAINER: Ra Daesung (daesungra@gmail.com)
 """
 
+import logging
 from datetime import timedelta
+from typing import Dict, Optional
 
 from apps.reservation.db.redis import REDIS
+from config import CONFIG
 
 
-TTL = timedelta(seconds=1800)
+LOGGER = logging.getLogger(__name__)
+REDIS_CONF = CONFIG['DB']['redis']
+TTL = timedelta(seconds=REDIS_CONF['ttl'])
 
 
 class ChatSession:
+    name: Optional[str] = None
+    chat_id: Optional[str] = None
+    username: Optional[str] = None
+    kwargs: Optional[Dict] = None
+
     def __init__(self, chat_id: str):
-        # TODO: Add selenium object variable to redis session
+        """Set and Get redis session info."""
+        if chat_id is None:
+            raise ValueError(f'[REDIS] No chat_id inserted.')
+
         self.name = f'chat_session_{chat_id}'
-        data = REDIS.hgetall(self.name)
-        self.username = data.get('username')
+        self.chat_id = chat_id
+
+        session_info = REDIS.hgetall(self.name)
+        if 'username' in session_info:
+            self.username = session_info.pop('username')
+            self.kwargs = {**session_info}
 
     def exists(self):
         return bool(REDIS.exists(self.name))
 
-    def commit(self):
+    def commit(self, **kwargs):
         """Update current data to redis server"""
+        assert self.username is not None
+
         REDIS.hmset(
             name=self.name,
             mapping={
+                'chat_id': self.chat_id,
                 'username': self.username,
-                # TODO: Add selenium object variable
+                **kwargs,
             },
         )
         self.touch()
+        LOGGER.info(f"[REDIS] {self.name} Updated.")
 
     def touch(self):
         """Reset TTL"""
@@ -39,3 +60,4 @@ class ChatSession:
     def delete(self):
         """Delete session"""
         REDIS.delete(self.name)
+        LOGGER.info(f"[REDIS] {self.name} Deleted..")
