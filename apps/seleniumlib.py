5cedfa3e52d3d3ccfd477903476e69506f46f635
daesungra <daesungra@gmail.com>
2021-07-24 23:52:01 +0900

    seleniumlib: Add search and action methods to driver handler

--- apps/seleniumlib.py
+++ apps/seleniumlib.py
@@ -4,11 +4,14 @@ MAINTAINER: Ra Daesung (daesungra@gmail.com)
 """
 
 import logging
+import functools
 from typing import Optional, Type
+from time import sleep
 
 from selenium.webdriver.remote.webdriver import WebDriver as RemoteWebDriver
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver import Chrome, ChromeOptions
-from selenium.common.exceptions import WebDriverException
+from selenium.common.exceptions import WebDriverException, UnexpectedAlertPresentException
 
 from config import CONFIG
 
@@ -58,7 +61,7 @@ class SeleniumHandler:
         """
         assert driver is not None
 
-        driver.implicitly_wait(5)
+        driver.implicitly_wait(time_to_wait=5)
         self.driver = driver
 
         if driver_not_found_error:
@@ -80,6 +83,21 @@ class SeleniumHandler:
         self.driver.get(open_url)
         LOGGER.info(f'URL opened by webdriver.')
 
+    def search_by_class_name(self, class_name: str): pass
+
+    def search_many_by_class_name(self, class_name: str): pass
+
+    def search_by_id(self, id_val: str): pass
+
+    def search_by_xpath(self, xpath: str): pass
+
+    def send_keys_to_elem(self, elem: WebElement, key: str): pass
+
+    def click_elem(self, elem: WebElement): pass
+
+    def sleep(self, sec: int):
+        sleep(sec)
+
     def quit(self):
         """Close opened web browser."""
         if self.driver is not None and isinstance(self.driver, RemoteWebDriver) and self.driver.current_url:
@@ -88,14 +106,67 @@ class SeleniumHandler:
             LOGGER.info(f"Webdriver is closed. current_url is '{curr_url}'")
 
 
+def deco_search_action(func):
+    """Decorate selenium action"""
+    @functools.wraps(func)
+    def _wrapper(self: SeleniumHandler, search_key: str, *args, **kwargs):
+        # TODO: Add more necessary pre_process
+        assert search_key is not None and isinstance(search_key, str)
+        return func(self, search_key, *args, **kwargs)
+    return _wrapper
+
+
+def deco_elem_action(func):
+    """Decorate selenium action"""
+    @functools.wraps(func)
+    def _wrapper(self: SeleniumHandler, elem: WebElement, *args, **kwargs):
+        # TODO: Add more necessary pre_process
+        assert elem is not None and isinstance(elem, WebElement)
+        try:
+            func(self, elem, *args, **kwargs)
+        except UnexpectedAlertPresentException as uae:
+            LOGGER.exception(f'Unexpected Alert Error occurred: {uae}')
+            self.driver.switch_to.alert.accept()
+            # Try again after alert clear
+            func(self, elem, *args, **kwargs)
+    return _wrapper
+
+
 class ChromeDriverHandler(SeleniumHandler):
     def __init__(self, url: str = None):
         """Set and Handling Chrome webdriver."""
         try:
             options = ChromeOptions()
             options.add_argument('window-size=1920,1080')
+            # options.add_argument('--headless')
+            # options.add_argument('--no-sandbox')
+            # options.add_argument('--disable-dev-shm-usage')
             driver = Chrome(executable_path=CHROME_DRIVER_PATH, options=options)
         except WebDriverException:
             raise WebDriverPathError(path=CHROME_DRIVER_PATH)
 
         super().__init__(driver=driver, driver_not_found_error=ChromeDriverNotFoundError, url=url)
+
+    @deco_search_action
+    def search_by_class_name(self, class_name: str):
+        return self.driver.find_element_by_class_name(class_name)
+
+    @deco_search_action
+    def search_many_by_class_name(self, class_name: str):
+        return self.driver.find_elements_by_class_name(class_name)
+
+    @deco_search_action
+    def search_by_id(self, id_val: str):
+        return self.driver.find_element_by_id(id_val)
+
+    @deco_search_action
+    def search_by_xpath(self, xpath: str):
+        return self.driver.find_element_by_xpath(xpath)
+
+    @deco_elem_action
+    def send_keys_to_elem(self, elem: WebElement, key: str):
+        elem.send_keys(key)
+
+    @deco_elem_action
+    def click_elem(self, elem: WebElement):
+        elem.click()
