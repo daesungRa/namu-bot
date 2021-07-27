d3d5663245fe30876a219a2ca14a8ab23765eaac
daesungra <daesungra@gmail.com>
2021-07-27 15:25:14 +0900

    reservation: Add yeyak facility flow

--- apps/reservation/yeyak.py
+++ apps/reservation/yeyak.py
@@ -70,3 +70,63 @@ class YeyakHandler(ChromeDriverHandler):
 
         LOGGER.info(f'[SEARCH] Done. search count of {len(options)}')
         return options
+
+    def yeyak_facility(self, facility_name: str, weektime: str, additional_word: str = None):
+        word = f', {additional_word}'
+        LOGGER.info(
+            f'[YEYAK] Start reservation with '
+            f'{facility_name}, {weektime}{additional_word and word or ""}...'
+        )
+
+        # Select facility type to soccer
+        soccer_code = 'T107'
+        self.action_select(self.search_by_xpath(YEYAK_CONF['xpath.select_facility_type']), soccer_code)
+        self.sleep(2)
+
+        # Select option by inserted param
+        select_elem = self.search_by_xpath(YEYAK_CONF['xpath.select_facilities'])
+        options = [
+            (option.get_property('value'), option.text)
+            for option in select_elem.find_elements_by_tag_name('option')
+            if facility_name in option.text and weektime in option.text
+        ]
+        if options:
+            # Select correct option with param
+            option_selected = options[0]
+            if len(options) > 1 and additional_word:
+                for o in options:
+                    if additional_word in o[1]:  # Compare with option text
+                        option_selected = o
+            LOGGER.info(f'[YEYAK] Select info > {option_selected}')
+            self.action_select(select_elem, option_selected[0])  # Select by option value
+
+            # Click 에약하기 main
+            self.click_elem(self.search_by_xpath('/html/body/div/div[3]/div[1]/div[1]/div/div/div[2]/div[1]/button'))
+
+            # Clear all popup
+            for popup in self.search_many_by_class_name('pop_x'):
+                LOGGER.info(f'[YEYAK] Click existing popup')
+                try:
+                    self.click_elem(popup)
+                except Exception:
+                    LOGGER.error(f'[YEYAK] An error occurred clicking popup button')
+
+            # Click 예약하기 second
+            self.click_elem(self.search_by_xpath('/html/body/div/div[3]/div[2]/div/form[2]/div[1]/div[2]/div/div/a[1]'))
+
+            # Select specific schedule
+            self.click_elem(self.search_many_by_class_name('able')[1].find_element_by_tag_name('a'))
+            self.click_elem(self.search_many_by_class_name('tab-all')[0].find_element_by_tag_name('a'))
+
+            # Additioanl info
+            self.click_elem(self.search_by_class_name('user_plus'))  # 이용인원
+            for e in self.search_many_by_class_name('book_tit2 label'):  # 신청자 정보와 동일, 전체동의
+                try:
+                    self.click_elem(e)
+                except Exception:
+                    LOGGER.error(f'[YEYAK] An error occurred clicking label for, "chk_info", "chk_agree_all"')
+            self.send_keys_to_elem(self.search_by_id('grp_nm'), 'hahaha')  # 단체명
+            self.send_keys_to_elem(self.search_by_id('form_email1'), 'hahahaha')  # 이메일1
+            self.send_keys_to_elem(self.search_by_id('form_email2'), 'gmail.com')  # 이메일2
+
+            self.sleep(3)
